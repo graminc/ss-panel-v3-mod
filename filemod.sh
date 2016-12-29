a46a381ea54e0d98f95576616d3b4a129c1b8077
huya1121 <huya1121@gmail.com>
2016-12-29 14:27:04 +0800

    新增bash脚本，自动下载fork代码并替换网站

---
+++ filemod.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+#请先自行配置好git仓库以及上游仓库。
+gitdir='git仓库配置目录'
+webdir='网站目录'
+githubadr='git@github.com:your_username/ss-panel-v3-mod.git'
+cd $gitdir
+git merge upstream/master
+git push origin master
+cd $webdir
+cp config/.config.php ~
+chattr -i public/.user.ini
+mv public/.user.ini .
+ls |grep -v filemod.sh |xargs rm -rf 
+git clone $githubadr 
+mv ss-panel-v3-mod/* .
+chown -R root:root *
+chmod -R 755 *
+chown -R www:www storage
+mv .user.ini public/
+chattr +i public/.user.ini
+cp ~/.config.php config/
+rm ss-panel-v3-mod -rf
